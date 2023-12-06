23c60b96c03aa92784a8bb422e48c8674bb007c5
jramir83 <32113776+jramir83@users.noreply.github.com>
2023-12-05 20:18:21 -0600

    Create Manage.js

---
+++ livedatalab_redesign/src/Manage.js
@@ -0,0 +1,19 @@
+import './Courses.css';
+
+function Courses() {
+  return (
+    <div class="background-box">
+    <div className="Courses">
+
+      <header className="Header">
+        <p class="is-size-5 has-text-weight-bold">Courses You Own</p>
+        <div className='subtext'>You do not own any courses.</div>
+        <p class="is-size-5 has-text-weight-bold">Projects You Own</p>
+        <div className='subtext'>You do not own any projects.</div>
+      </header>
+    </div>
+    </div>
+  );
+}
+
+export default Courses;
