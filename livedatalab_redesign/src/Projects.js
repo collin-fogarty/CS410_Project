4b0b6f5d7899b74b543d3e33a3c3c554c5261310
jramir83 <32113776+jramir83@users.noreply.github.com>
2023-12-05 20:15:37 -0600

    Update Projects.js

--- livedatalab_redesign/src/Projects.js
+++ livedatalab_redesign/src/Projects.js
@@ -1,26 +1,12 @@
-import logo from './cornerlogo.png';
 import './Projects.css';
 
 function Projects() {
   return (
     <div className="Projects">
 
-      <nav className='NavBar' role='navigation'>
-
-        <div className='Pages'>
-          <img src={logo} className="LiveDataLabLogo" alt="logo" />
-          <a class="navbar-item" href="/">Home</a>
-          <a class="navbar-item" href="/projects">Projects</a>
-          <a class="navbar-item" href="/courses">Courses</a>
-          <a class="navbar-item" href="/manage">Manage</a>
-          <a class="navbar-item" href="/create">Create</a>
-        </div>
-
-      </nav>
-
       <header className="Header">
-        <p>Your Projects</p>
-        <div className='subtext'>No projects started</div>
+        <p>Projects You've Started</p>
+        <div className='subtext'>You have not started any projects.</div>
       </header>
 
     </div>
