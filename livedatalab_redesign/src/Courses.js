bfffc274bbdd5711bdd9b492b74e4f67f00f68f0
jramir83 <32113776+jramir83@users.noreply.github.com>
2023-12-05 20:14:52 -0600

    Create Courses.js

---
+++ livedatalab_redesign/src/Courses.js
@@ -0,0 +1,19 @@
+import './Courses.css';
+
+function Courses() {
+  return (
+    <div class="background-box">
+    <div className="Courses">
+
+      <header className="Header">
+        <p class="is-size-5 has-text-weight-bold">Courses You're Enrolled In</p>
+        <div className='subtext'>You are not enrolled in any courses.</div>
+        <p>All Courses</p>
+        <div className='subtext'>You are not enrolled in any courses.</div>
+      </header>
+    </div>
+    </div>
+  );
+}
+
+export default Courses;
