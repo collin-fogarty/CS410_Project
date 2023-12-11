cc94b7bd68e231762c13c9c2d216278dfb88d8f7
collin-fogarty <fogarty4@illinois.edu>
2023-12-10 19:44:18 -0600

    Changes to navbar and projects page

--- livedatalab_firebase/src/pages/projects/index.jsx
+++ livedatalab_firebase/src/pages/projects/index.jsx
@@ -8,23 +8,36 @@ export const Projects = () => {
     <div className="Projects">
        {<Navbar />}
 
-      <header className="Header">
-      <div className="projects">
-        <p>Your Projects</p>
-            <ul>
-              {projects.map((project) => {
-                const { projectName, projectDesc, course } = project;
-
-                return (
-                  <li>
-                <p>{projectName}: {projectDesc}</p>                  </li>
-                );
-              })}
-            </ul>
-          </div>
+      <header className="ProjectsHeader">
+        <div className = "Title">
+          <p>Projects you've started</p>
+        </div>
         
-      </header>
+        <div className="List">
+          <ul>
+            {projects.map((project) => {
+              const { projectName, projectDesc} = project;
 
+              return (
+                <li className='ListedProject'>
+                  <p><span className='emphasized'>{projectName}: </span>{projectDesc} | 
+                  <span className='emphasized'> Rank: </span>4 | 
+                  <span className='emphasized'> Username: </span>github-username | 
+                  <span className='emphasized'> Submission Number: </span>4 | 
+                  <span className='emphasized'> Score: </span>1</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className='SecondTitle'>
+            <p>Joined Projects</p>
+            <p><span className='SecondSubtext'>You haven't joined any outside projects</span></p>
+        </div>
+
+      </header>
+    
     </div>
   );
 };
