a3f802ff65235c5b3921d7c2059cef71558916e9
Seth Buchinger <seth.buchinger@icloud.com>
2023-12-12 06:27:27 -0500

    integrated projec/coures info into Manage page

--- livedatalab_firebase/src/pages/manage/index.jsx
+++ livedatalab_firebase/src/pages/manage/index.jsx
@@ -1,7 +1,13 @@
 import './styles.css';
 import {Navbar} from "../../components/Navbar";
+import { useGetProjects } from "../../hooks/useGetProjects";
+import { useGetCourses } from "../../hooks/useGetCourses"
 
 export const Manage = () => {
+
+
+  const { projects } = useGetProjects();
+  const { courses } = useGetCourses();
   return (
     <div class="background-box">
     <div className="Courses">
@@ -9,9 +15,34 @@ export const Manage = () => {
 
       <header className="Header">
         <p class="is-size-5 has-text-weight-bold">Courses You Own</p>
-        <div className='subtext'>You do not own any courses.</div>
+        
+        <div className="courses">
+            <ul>
+              {courses.map((courses) => {
+                const { courseName, courseDesc} = courses;
+
+                return (
+                  <li>
+            <p>{courseName}: {courseDesc}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         <p class="is-size-5 has-text-weight-bold">Projects You Own</p>
-        <div className='subtext'>You do not own any projects.</div>
+        <div className="projects">
+            <ul>
+              {projects.map((project) => {
+                const { projectName, projectDesc, course } = project;
+
+                return (
+                  <li>
+            <p>{projectName}: {projectDesc}: {course} </p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
       </header>
     </div>
     </div>
