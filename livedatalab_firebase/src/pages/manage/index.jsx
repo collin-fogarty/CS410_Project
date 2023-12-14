5c2569055a7df0d791034eec756618a648df7791
jramir83 <32113776+jramir83@users.noreply.github.com>
2023-12-14 00:48:18 -0600

    Update index.jsx

--- livedatalab_firebase/src/pages/manage/index.jsx
+++ livedatalab_firebase/src/pages/manage/index.jsx
@@ -1,50 +1,76 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 import {Navbar} from "../../components/Navbar";
-import { useGetProjects } from "../../hooks/useGetProjects";
-import { useGetCourses } from "../../hooks/useGetCourses"
+import { useGetCourses } from "../../hooks/useGetCourses";
+import { useGetProjects } from '../../hooks/useGetProjects';
 
 export const Manage = () => {
-
-
-  const { projects } = useGetProjects();
   const { courses } = useGetCourses();
+  const { projects } = useGetProjects();
   return (
-    <div class="background-box">
-    <div className="Courses">
-    {<Navbar />}
+    
+    <div className="Manage">
+       {<Navbar />}
 
-      <header className="Header">
-        <p class="is-size-5 has-text-weight-bold">Courses You Own</p>
+      <header className="ProjectsHeader">
+
+        <div className = "Title">
+          <p>Courses You Own</p>
+        </div>
         
-        <div className="courses">
-            <ul>
-              {courses.map((courses) => {
-                const { courseName, courseDesc} = courses;
+        <ul>
+          {courses.length > 0 ? (
+            <div className="List">
+              {courses.map((course) => (
+                <li className='ListedCourse' key={course.courseID} style={{ textAlign: 'left'}}>
+                  <Link to={`/coursesPage/${course.courseName}`} style={{ textDecoration: 'none' }}>
+                    <p><span className='emphasized'>{course.courseName}: </span>{course.courseDesc}</p>
+                  </Link>
+                </li>
+              ))}
+            </div>
+          ) : (
+            <div>
+              <div className="course-card" style={{ textAlign: 'left', fontSize: '18px' }}>
+                You are not enrolled in any courses.
+              </div>
+            </div>
+          )}
+        </ul>
 
-                return (
-                  <li>
-            <p>{courseName}: {courseDesc}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        <p class="is-size-5 has-text-weight-bold">Projects You Own</p>
-        <div className="projects">
-            <ul>
-              {projects.map((project) => {
-                const { projectName, projectDesc, course } = project;
+        <div className = "SecondTitle">
+          <p>Projects You Own</p>
+        </div>
+        
+        {projects.length > 0 ? (
+          <div className="List">
+          <ul>
+            {projects.map((project) => {
+              const { projectName, projectDesc} = project;
 
-                return (
-                  <li>
-            <p>{projectName}: {projectDesc}: {course} </p>
-                  </li>
-                );
-              })}
-            </ul>
+              return (
+                <li className='ListedProject'>
+                  <p><span className='emphasized'>{projectName}: </span>{projectDesc} | 
+                  <span className='emphasized'> Rank: </span>4 | 
+                  <span className='emphasized'> Username: </span>github-username | 
+                  <span className='emphasized'> Submission Number: </span>4 | 
+                  <span className='emphasized'> Score: </span>1</p>
+                </li>
+              );
+            })}
+          </ul>
           </div>
+        ) : (
+          <div>
+              <div className="course-card" style={{ textAlign: 'left', fontSize: '18px' }}>
+                You do not own any projects.
+              </div>
+            </div>
+        )}
+
       </header>
-    </div>
+    
     </div>
   );
 };
