0cb477dc916cc8fd0dc7b5f857f858cdff091e2f
jramir83 <32113776+jramir83@users.noreply.github.com>
2023-12-14 00:50:02 -0600

    Update index.jsx

--- livedatalab_firebase/src/pages/courses/index.jsx
+++ livedatalab_firebase/src/pages/courses/index.jsx
@@ -1,40 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
-import {Navbar} from "../../components/Navbar";
-import { useGetCourses } from "../../hooks/useGetCourses";
+import { Navbar } from '../../components/Navbar';
+import { useGetCourses } from '../../hooks/useGetCourses';
 
-export const Courses = () =>{
-
+export const Courses = () => {
   const { courses } = useGetCourses();
+
   return (
-    
-    <div class="background-box">
-  
     <div className="Courses">
-    {<Navbar />}
+      <Navbar />
 
-      <header className="Header">
-        <p class="is-size-5 has-text-weight-bold">Courses You're Enrolled In</p>
-        <div className="courses">
-        <ul>
-          {courses.map((course) => {
-            const { courseName, courseDesc} =
-              course;
-
-            return (
-              <li>
-  
-                <p>{courseName}-{courseDesc}</p>
+      <header className="ProjectsHeader">
+        <div className="Title">
+          <p>Courses You're Enrolled In</p>
+        </div>
         
-              </li>
-            );
-          })}
+        <ul>
+          {courses.length > 0 ? (
+            <div className="List">
+              {courses.map((course) => (
+                <li className='ListedCourse' key={course.courseID} style={{ textAlign: 'left'}}>
+                  <Link to={`/coursesPage/${course.courseName}`} style={{ textDecoration: 'none' }}>
+                    <p><span className='emphasized'>{course.courseName}: </span>{course.courseDesc}</p>
+                  </Link>
+                </li>
+              ))}
+            </div>
+          ) : (
+            <div>
+              <div className="course-card" style={{ textAlign: 'left', fontSize: '18px' }}>
+                You are not enrolled in any courses.
+              </div>
+            </div>
+          )}
         </ul>
-      </div>
-        <p>All Courses</p>
-        <div className='subtext'>You are not enrolled in any courses.</div>
+      
+        <div className="SecondTitle">
+          <p>All Courses</p>
+        </div>
+        
+        <ul>
+          {courses.length > 0 ? (
+            <div className="List">
+              {courses.map((course) => (
+                <li className='ListedCourse' key={course.courseID} style={{ textAlign: 'left'}}>
+                  <Link to={`/coursesPage/${course.courseName}`} style={{ textDecoration: 'none' }}>
+                    <p><span className='emphasized'>{course.courseName}: </span>{course.courseDesc}</p>
+                  </Link>
+                </li>
+              ))}
+            </div>
+          ) : (
+            <div>
+              <div className="course-card" style={{ textAlign: 'left', fontSize: '18px' }}>
+                There are no courses available.
+              </div>
+            </div>
+          )}
+        </ul>
       </header>
     </div>
-    </div>
   );
-}
-
+};
